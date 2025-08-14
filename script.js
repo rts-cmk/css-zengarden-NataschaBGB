17ae7a29d0d01fab0b7a14c16793daaf1e81e1e7
NataschaBGB <nannabjerning@hotmail.com>
2025-08-14 10:34:53 +0200

    added comments to js file

--- script.js
+++ script.js
@@ -1,47 +1,77 @@
-// function toggleTheme() {
-//     let style = document.getElementById('style');
+// // ------------ FUNCTION 1 ------------
     
-//     if (style.getAttribute('href') == 'finalending.css') {
-//         style.setAttribute('href', 'silentstrength.css');
+//     // This function toggles between three themes by checking the current href attribute of the link element with id 'style'.
+//     // It only uses one link element with id 'style' to change the stylesheet.
+    
+//     function toggleTheme() {
+//         // Get the link element with id 'style'
+//         let style = document.getElementById('style');
+        
+//         // Check the current href attribute of the link element
+//         // If the href is 'finalending.css', a click on the button will change it to 'silentstrength.css'.
+//         if (style.getAttribute('href') == 'finalending.css') {
+//             style.setAttribute('href', 'silentstrength.css');
+//         }
+//         // If the href is 'wilderness.css', a click on the button will change it to 'finalending.css'.
+//         else if (style.getAttribute('href') == 'wilderness.css') {
+//             style.setAttribute('href', 'finalending.css');
+//         }
+//         // If the href is 'silentstrength.css', a click on the button will change it to 'wilderness.css'.
+//         else if (style.getAttribute('href') == 'silentstrength.css') {
+//             style.setAttribute('href', 'wilderness.css');
+//         }
+//         // If the href is not one of the expected values, it defaults to 'finalending.css'.
+//         else {
+//             style.setAttribute('href', 'finalending.css');
+//         }
 //     }
-//     else if (style.getAttribute('href') == 'silentstrength.css') {
-//         style.setAttribute('href', 'wilderness.css');
+
+// // ------------ FUNCTION 2 ------------
+
+//     // This function toggles between three themes by cycling through an array of theme names.
+//     // It only uses one link element with id 'style' to change the stylesheet.
+//     // create an array of theme names
+//     // The themes are the names of the CSS files that will be used for styling.
+//     const themes = [
+//         'wilderness.css',
+//         'silentstrength.css',
+//         'finalending.css'
+//     ];
+
+//     function toggleTheme() {
+//         // Get the link element with id 'style'
+//         const style = document.getElementById('style');
+//         // Get the current href attribute of the link element
+//         const currentTheme = style.getAttribute('href');
+
+//         // The indexOf method returns the position of the current theme in the themes array.
+//         let index = themes.indexOf(currentTheme);
+
+//         // The (index + 1) expression calculates the next index (theme) in the array.
+//         // The % operator ensures that if we reach the end of the array, we loop back to the start.
+//         index = (index + 1) % themes.length;
+
+//         // Set the href attribute of the link element to the next theme in the array
+//         style.setAttribute('href', themes[index]);
 //     }
-//     else {
-//         style.setAttribute('href', 'finalending.css');
-//     }
-// }
 
-// const themes = [
-//     'wilderness.css',
-//     'silentstrength.css',
-//     'finalending.css'
-// ];
+// ------------ FUNCTION 3 ------------
+    
+    // This function toggles between multiple stylesheets ind index file, by disabling the current one and enabling the next one in the list.
+    
+    function toggleTheme() {
+        // gets all links tags that are stylesheets and converts them to an array
+        const stylesheets = Array.from(document.querySelectorAll('link[rel="stylesheet"]'));
+        // returns first index of stylesheet that is not disabled
+        const activeIndex = stylesheets.findIndex(sheet => !sheet.disabled);
 
-// function toggleTheme() {
-//     const style = document.getElementById('style');
-//     const currentTheme = style.getAttribute('href');
+        // sets the currently active stylesheet to disabled
+        stylesheets[activeIndex].disabled = true;
 
-//     // Find index for nuværende tema
-//     let index = themes.indexOf(currentTheme);
+        // activeIndex + 1 gives the next stylesheet in the array.
+        // % stylesheets.length ensures that if we reach the end, we start over (loop).
+        const nextIndex = (activeIndex + 1) % stylesheets.length;
 
-//     // Beregn næste index (loop rundt til start)
-//     index = (index + 1) % themes.length;
-
-//     // Skift til næste tema
-//     style.setAttribute('href', themes[index]);
-// }
-
-function toggleTheme() {
-    const stylesheets = Array.from(document.querySelectorAll('link[rel="stylesheet"]'));
-    const activeIndex = stylesheets.findIndex(sheet => !sheet.disabled);
-
-    // Deaktiver nuværende
-    stylesheets[activeIndex].disabled = true;
-
-    // Beregn næste index
-    const nextIndex = (activeIndex + 1) % stylesheets.length;
-
-    // Aktivér næste
-    stylesheets[nextIndex].disabled = false;
-}
+        // remove disabled attribute from the next stylesheet and apply it
+        stylesheets[nextIndex].disabled = false;
+    }
